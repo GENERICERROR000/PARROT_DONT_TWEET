716963dafd21d8c393f359fa96477962d548bb4e
Noah K <nkernis@gmail.com>
2018-10-13 19:28:14 -0400

    fix for empty string error from DynamoDb

--- controllers/twit.js
+++ controllers/twit.js
@@ -12,22 +12,36 @@ module.exports = () => {
   // Start Listening To Twitter Stream
   const stream = Twitter.stream('statuses/filter', { follow: [idToParrot, idParrot], filter_level: 'low' })
 
+  const removeEmptyStringElements = (obj) => {
+    for (var prop in obj) {
+      if (typeof obj[prop] === 'object') {// dive deeper in
+        removeEmptyStringElements(obj[prop])
+      } else if(obj[prop] === '') {// delete elements that are empty strings
+        obj[prop] = 'EMPTY_STRING_FIX'
+      }
+    }
+    console.log(obj);
+    return obj
+  }
+
   // Action to take when a tweet occurs
   stream.on('tweet', (tweet) => {
-    switch (tweet.user.id_str) {
+    const cleanedTweet = removeEmptyStringElements(tweet)
+
+    switch (cleanedTweet.user.id_str) {
       case idToParrot: // If tweet is from User being followed
-        saveTweet(idToParrot, tweet)
-        console.log("TWEET TO PARROT:", tweet)
+        saveTweet(idToParrot, cleanedTweet)
+        console.log("TWEET TO PARROT:", cleanedTweet)
 
         // Post a new tweet with text from received tweet
-        Twitter.post('statuses/update', { status: tweet.text }, (err, data, response) => {
+        Twitter.post('statuses/update', { status: cleanedTweet.text }, (err, data, response) => {
           if (err) console.error("Error posting Parrot Tweet:", err)
         })
         break
 
       case idParrot: // If tweet is from Parroting acount
-        saveTweet(idParrot, tweet)
-        console.log("PARROT TWEETED:", tweet);
+        saveTweet(idParrot, cleanedTweet)
+        console.log("PARROT TWEETED:", cleanedTweet);
         break
     }
   })
